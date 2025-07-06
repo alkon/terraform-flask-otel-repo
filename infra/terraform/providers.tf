089fcd981e40adc79dbf97de066991f9915638bb
alkon <aikbox@yahoo.com>
2025-07-06 21:32:22 +0300

    Fix auto-injection of instrumentation

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -47,16 +47,10 @@ provider "kubectl" {
   config_context = "k3d-${data.external.k3d_cluster_bootstrap.result.cluster_name}"
 }
 
-# provider "argocd" {
-#   alias   = "main"
-#   server_addr = var.argocd_server
-#   auth_token  = var.argocd_auth_token
-#   insecure    = var.argocd_insecure
-# }
 provider "argocd" {
-  alias       = "main"
-  server_addr = "localhost:30080"
-  auth_token  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJhZG1pbjphcGlLZXkiLCJuYmYiOjE3NTE1NTkxOTYsImlhdCI6MTc1MTU1OTE5NiwianRpIjoiYzg0MDgzNDMtMDY1My00NTVmLTlhNmItZmZjYjU1ZWMxNzc4In0.uYBdMWZbOKVWaghjqXXp4rCTehlQLGZcEV-2NEyr2Sc"
-  insecure    = true
+  alias   = "main"
+  server_addr = var.argocd_server
+  auth_token  = var.argocd_auth_token
+  insecure    = var.argocd_insecure
   plain_text  = true # no need in variable for this
 }
